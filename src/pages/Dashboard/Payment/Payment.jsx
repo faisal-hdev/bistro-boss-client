42a56a9ce95a051c4b1ebd275e5272da58b92717
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-08-30 18:46:05 +0600

    Checkout form created and use them

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+// TODO : Add publishable key
+
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
   return (
     <div className="w-full">
       <SectionTitle heading="Payment" subHeading="Please pay to eat" />
       <div>
-        <h2 className="text-4xl font-bold text-center">Tk in my Pocket</h2>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
       </div>
     </div>
   );
